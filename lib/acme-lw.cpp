0faa42dd9716104d2006e77dc8c0c8bcbc621362
jmccl <jmccl@users.noreply.github.com>
2019-11-29 09:40:11 -0800

    Minor optimization

--- lib/acme-lw.cpp
+++ lib/acme-lw.cpp
@@ -539,6 +539,7 @@ struct AcmeClientImpl
                         callback(domain, url, keyAuthorization);
 
                         verifyChallengePassed(challenge);
+                        continue;
                     }
                 }
             }
